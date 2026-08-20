4e533a95968b4dc7e85d7938972b8a3b00d15b1a
Darko Mitrev <darkomitrev@Darkos-MacBook-Air.local>
2026-08-20 06:54:25 +0200

    track

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
         <>
           <ScrollNavbar />
           <HeroSplit />
-          <LogoCloudAnimated />
           <AboutSection />
           <Highlights />
           <FinancingPartners />
