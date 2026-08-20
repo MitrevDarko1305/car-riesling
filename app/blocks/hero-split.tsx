81ef698461d1efe425ba50b72b0f3f2bd4abfea9
Darko Mitrev <darkomitrev@Darkos-MacBook-Air.local>
2026-08-20 08:41:24 +0200

    ciao

--- app/blocks/hero-split.tsx
+++ app/blocks/hero-split.tsx
@@ -23,22 +23,22 @@ export default function HeroSplit() {
   return (
     <section className="relative flex flex-col md:flex-row min-h-[calc(85vh)] pt-24 w-full bg-black overflow-hidden">
       {/* Left: Text */}
-      <div className="flex flex-col justify-center px-6 md:px-16 lg:px-24  md:w-1/2 z-10 min-w-0">
-        <span className="text-red-600 font-medium tracking-wide capitalise text-sm mb-2">
+      <div className="flex flex-col justify-center px-3 md:px-16 lg:px-24  md:w-1/2 z-10 min-w-0">
+        <span className="text-accent font-medium tracking-wide  text-sm mb-2">
           Premium Cars, Stuttgart
         </span>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-light leading-tight mb-6">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-light leading-[1] md:leading-tight md:mb-6 mb-3">
           Fulfill your dreams,<br />drive in style.
         </h1>
-        <p className="text-light/50 text-lg mb-8 max-w-md">
+        <p className="text-light/50 md:text-lg text-[14px] mb-8 max-w-md">
           Hand-picked luxury and performance vehicles, backed by a team that
           treats every sale like it&apos;s their own car.
         </p>
-        <div className="flex gap-4">
-          <button className="bg-red-700 hover:bg-black cursor-pointer font-black text-light px-8 py-3 r transition-colors">
+        <div className="flex flex-col md:flex-row gap-4">
+          <button className="bg-red-700  hover:bg-black cursor-pointer text-[13px] md:text-lg font-black text-light px-8 md:py-3 py-2 r transition-colors">
             Browse Inventory
           </button>
-          <button className="border border-white/15 cursor-pointer hover:border-white text-light/50 hover:text-light px-8 py-3 transition-colors">
+          <button className="border border-white/15 cursor-pointer text-[13px] md:text-lg hover:border-white text-light/50 hover:text-light px-8 py-2 md:py-3 transition-colors">
             See Gallery
           </button>
         </div>
